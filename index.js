4cfe20befc3042bce74aa27cc40d16d4b682e61a
Eric Gonzalez <ejoglez@gmail.com>
2022-01-04 18:34:04 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const sel = document.querySelector('#main');
+sel.remove();
+
+const newHeader = document.createElement('h1');
+//const newHeaderText = document.createTextNode('s the Champion');
+
+newHeader.id = 'victory';   
+newHeader.innerHTML = ('E is the champion');
